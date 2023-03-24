5d9ee41c12ad67844a1c5a7fcff5ffaa84f05098
Tomas Tisocco <tomastisocco97@gmail.com>
2023-03-24 00:53:02 -0300

    Add retry parameters

--- lib/src/tus_client_base.dart
+++ lib/src/tus_client_base.dart
@@ -27,6 +27,9 @@ abstract class TusClientBase {
     this.file, {
     this.store,
     this.maxChunkSize = 512 * 1024,
+    this.retries = 0,
+    this.retryScale = RetryScale.constant,
+    this.retryInterval = 0,
   });
 
   /// Create a new upload URL
@@ -90,6 +93,43 @@ abstract class TusClientBase {
   /// The maximum payload size in bytes when uploading the file in chunks (512KB)
   final int maxChunkSize;
 
+  /// The number of times that should retry to resume the upload if a failure occurs after rethrow the error.
+  final int retries;
+
+  /// The interval between the first error and the first retry in [seconds].
+  final int retryInterval;
+
+  /// The scale type used to increase the interval of time between every retry.
+  final RetryScale retryScale;
+
   /// Whether the client supports resuming
   bool get resumingEnabled => store != null;
+}
+
+enum RetryScale {
+  /// Same time interval between every retry.
+  constant,
+
+  /// If interval is n, on every retry the the interval is increased by n.
+  /// For example if [retryInterval] is set to 2 seconds, and the [retries] is set to 4,
+  /// the interval for every retry is going to be [2, 4, 6, 8]
+  lineal,
+
+  /// If interval is n, on every retry the last interval is going to be duplicated.
+  /// For example if [retryInterval] is set to 2 seconds, and the [retries] is set to 4,
+  /// the interval for every retry is going to be [2, 4, 8, 16]
+  exponential;
+
+  Duration getInterval(int retry, int retryInterval) {
+    if (retryInterval == 0) return Duration.zero;
+    if (retry == 0) return Duration(seconds: retryInterval);
+    switch (this) {
+      case RetryScale.constant:
+        return Duration(seconds: retryInterval);
+      case RetryScale.lineal:
+        return Duration(seconds: (retry + 1) * retryInterval);
+      case RetryScale.exponential:
+        return Duration(seconds: retryInterval * ((2) ^ (retry + 1)));
+    }
+  }
 }
