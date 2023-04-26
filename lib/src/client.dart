19bb1ce3b1be0d50da675d66251d0fda39023f32
Tomas Tisocco <tomastisocco97@gmail.com>
2023-04-25 22:43:50 -0300

    fix: analysis warnings (#10)

--- lib/src/client.dart
+++ lib/src/client.dart
@@ -1,7 +1,7 @@
 import 'dart:async';
 import 'dart:developer';
 import 'dart:io';
-import 'dart:math' show min, pow;
+import 'dart:math' show min;
 import 'dart:typed_data' show Uint8List, BytesBuilder;
 import 'package:speed_test_dart/speed_test_dart.dart';
 import 'package:tus_client_dart/src/retry_scale.dart';
